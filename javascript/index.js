6ef94a3ca7018481cef5950c5616f943fd8afc25
sameer shaik <81900840+sameershaik-tech@users.noreply.github.com>
2022-07-28 21:23:41 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -3,4 +3,4 @@ function reverse_a_number(n)
 	n = n + "";
 	return n.split("").reverse().join("");
 }
-console.log(Number(reverse_a_number(32243)));
+document.write(Number(reverse_a_number(32243)));
